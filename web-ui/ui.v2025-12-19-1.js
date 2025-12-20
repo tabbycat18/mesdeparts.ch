7fde6af19fc4af7cfe6e90c3099327cd56afaf85
tabbycat18 <m.pastore.ch+github@protonmail.com>
2025-12-20 16:37:46 +0100

    Updated file version

--- web-ui/ui.v2025-12-19-1.js
+++ web-ui/ui.v2025-12-19-1.js
@@ -11,21 +11,21 @@ import {
   API_MODE_DIRECT,
   API_MODE_STORAGE_KEY,
   API_MODE_AUTO_OFF_KEY,
-} from "./state.v2025-12-19.js";
+} from "./state.v2025-12-19-1.js";
 import {
   fetchStationSuggestions,
   fetchStationsNearby,
   fetchJourneyDetails,
   parseApiDate,
-} from "./logic.v2025-12-19.js";
+} from "./logic.v2025-12-19-1.js";
 import {
   loadFavorites,
   addFavorite,
   removeFavorite,
   isFavorite,
   clearFavorites,
-} from "./favourites.v2025-12-19.js";
-import { t } from "./i18n.v2025-12-19.js";
+} from "./favourites.v2025-12-19-1.js";
+import { t } from "./i18n.v2025-12-19-1.js";
 
 const QUICK_CONTROLS_STORAGE_KEY = "mesdeparts.quickControlsCollapsed";
 let quickControlsCollapsed = false;
