e7f66b03309e62cfff07909cb87fcb5214995e26
tabbycat18 <tabbycat18@proton.me>
2026-01-03 20:48:57 +0100

    v2026-01-03-11

--- web-ui/test/logic.test.js
+++ web-ui/test/logic.test.js
@@ -4,7 +4,7 @@ import {
   classifyMode,
   detectNetworkFromStation,
   parseApiDate,
-} from "../logic.v2026-01-03-10.js";
+} from "../logic.v2026-01-03-11.js";
 
 // classifyMode should categorize common transport codes
 assert.equal(classifyMode("IC"), "train");
